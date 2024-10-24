2006e64b0061a0c2779824e6ab0754385463ebe6
Sebastian Roldan <sebasroldanm@gmail.com>
2024-10-23 20:09:57 -0500

    Ft. Live

---
+++ resources/views/livewire/owner/live.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <video id="my_video_1" style="max-height: 85vh;" class="video-js vjs-default-skin" {{-- width="740px" height="420px" --}} controls
+        preload="none" data-setup='{}'>
+    </video>
+</div>
