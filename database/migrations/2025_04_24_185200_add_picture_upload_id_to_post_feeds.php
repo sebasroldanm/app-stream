bf7da93dcc9a00c2648699865424b72ec58bd875
Sebastian Roldan <sebasroldanm@gmail.com>
2025-04-24 19:37:40 -0500

    Update migrations new column (Service Img Upload)

---
+++ database/migrations/2025_04_24_185200_add_picture_upload_id_to_post_feeds.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('post_feeds', function (Blueprint $table) {
+            $table->string('picture_upload_id', 10);
+            $table->index('picture_upload_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('post_feeds', function (Blueprint $table) {
+            $table->dropIndex(['picture_upload_id']);
+            $table->dropColumn('picture_upload_id');
+        });
+    }
+};
