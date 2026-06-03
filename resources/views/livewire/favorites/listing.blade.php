32cad1a40797df237af712e671ff38ff6c7fce3e
Sebastian Roldan <sroldan@zafirus.tech>
2026-06-03 07:58:27 -0500

    feat: add 24h filter to chat service, categorize live favorites, and display empty state in chat list

--- resources/views/livewire/favorites/listing.blade.php
+++ resources/views/livewire/favorites/listing.blade.php
@@ -1,32 +1,38 @@
 <div>
     <div class="row">
+        {{-- Variable para controlar el cambio de sección --}}
+        @php $isCurrentSectionLive = null; @endphp
+
         @foreach ($favorites as $favorite)
+            @if ($isCurrentSectionLive !== (bool)$favorite->isLive)
+                @php $isCurrentSectionLive = (bool)$favorite->isLive; @endphp
+                
+                <div class="col-12">
+                    <div class="iq-card">
+                        <div class="iq-card-header d-flex justify-content-between">
+                            <div class="iq-header-title">
+                                <h4 class="card-title">{{ $isCurrentSectionLive ? 'En vivo' : 'Favoritos' }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
             <div class="col-4 col-md-3 col-lg-2 mb-2" wire:key="fav-owner-{{ $favorite->id }}">
                 <x-ownerInfoCard 
-                    :primaryImage="$favorite->isLive
-                    ? 'https://img.doppiocdn.net/thumbs/' .
-                        $favorite->data->user->user->snapshotTimestamp .
-                        '/' .
-                        $favorite->id
-                    : null" 
+                    :primaryImage="$favorite->isLive ? 'https://img.doppiocdn.net/thumbs/' . $favorite->data->user->user->snapshotTimestamp . '/' . $favorite->id : null" 
                     :secondaryImage="$favorite->pic_profile" 
                     :isMobile="$favorite->data->user->user->isMobile" 
                     :username="$favorite->username"
                     :idOwner="$favorite->id" 
-                    :settings="[
-                        'autoplay' => false,
-                        'allowTouchMove' => true,
-                        'simulateTouch' => true,
-                    ]" 
+                    :settings="['autoplay' => false, 'allowTouchMove' => true, 'simulateTouch' => true]" 
                     :status="$favorite->general_condition" 
                     :viewersCount="$favorite->stat?->viewers"
                     :isLive="$favorite->isLive" 
-                    :lastLive="!$favorite->isLive
-                        ? \Carbon\Carbon::parse($favorite->statusChangedAt)->diffForHumans(['short' => true])
-                        : null"
+                    :lastLive="!$favorite->isLive ? \Carbon\Carbon::parse($favorite->statusChangedAt)->diffForHumans(['short' => true]) : null"
                     :lastGoal="$favorite->latestGoal?->getPercentage()" 
-                    :lastGoalDescription="$favorite->latestGoal?->description" />
+                    :lastGoalDescription="$favorite->latestGoal?->description" 
+                />
             </div>
         @endforeach
     </div>
-</div>
--- resources/views/livewire/owner/live/chat.blade.php
+++ resources/views/livewire/owner/live/chat.blade.php
@@ -1,81 +1,91 @@
 <div class="card-body p-2" wire:poll.3s.visible>
     <ul class="iq-timeline p-2">
-        @foreach ($messages as $message)
-            <li class="{{ $message->isNew ? 'newMessage' : '' }}" wire:key="{{ $message->id }}">
-                @if (($message->userData['isModel'] ?? false) && $message->owner_id === ($message->userData['id'] ?? null))
-                    @if (isset($message->details['lovenseDetails']))
-                        <div class="timeline-dots border-danger"></div>
-                    @elseif(isset($message->details['goal']))
-                        <div class="timeline-dots border-info"></div>
-                    @else
-                        <div class="timeline-dots border-success"></div>
-                    @endif
-                @else
-                    <div class="timeline-dots"></div>
-                @endif
-                <div class="d-flex align-items-center justify-content-between">
-                    @if (isset($message->details['isAnonymous']) && $message->details['isAnonymous'])
-                        <h6 class="mb-1">Anonymous</h6>
-                    @else
-                        <h6 class="mb-1">{{ $message->userData['username'] ?? 'Anonymous' }}</h6>
-                    @endif
-                    <a href="{{ route('metadata', ['model' => 'chat', 'id' => base64_encode(json_encode($message->toArray()))]) }}"
-                        target="_blank">
-                        <small>{{ $message->elapsedTime }}</small>
-                    </a>
-                </div>
-                <div class="d-inline-block w-100">
-                    {{-- Mensaje owner --}}
-                    @if ($message->userData['isModel'] ?? false)
-                        {{-- Peticion Owner --}}
-                        @if (isset($message->details['goal']))
-                            <p>Objetivo: {{ $message->details['body'] ?? '' }} - {{ $message->details['goal'] }}</p>
-                            {{-- Respuesta owner --}}
-                        @elseif (isset($message->details['body']))
-                            <p>{{ $message->details['body'] }}</p>
-                            {{-- API --}}
-                        @elseif(isset($message->details['lovenseDetails']))
-                            @php
-                                $love = $message->details['lovenseDetails']['detail'];
-                            @endphp
-                            @if (isset($love['power']))
-                                <p>{{ $love['power'] }}({{ $love['time'] }} sec.) por <bold>{{ $love['name'] }}</bold>
-                                    - propina {{ $love['amount'] }}</p>
-                            @elseif(isset($love['specialActualValue']) && $love['specialActualValue'] == 'giveControl')
-                                <p>Control por {{ $love['name'] }}</p>
-                            @else
-                                <p>Propina {{ $love['amount'] ?? dd($love) }} ({{ $love['time'] }} sec.)
-                                    {{ $love['name'] }}</p>
-                            @endif
-                            {{-- Propina --}}
-                        @else
-                            <p>Propina {{ $message->details['amount'] ?? '' }}</p>
-                        @endif
-                        {{-- Mensaje Regular --}}
-                    @else
-                        {{-- Propina --}}
-                        @if (isset($message->details['body']) && $message->details['body'] == '' && isset($message->details['amount']))
-                            <p>Propina {{ $message->details['amount'] }}</p>
-                            {{-- Nuevo King --}}
-                        @elseif($message->type == 'newKing')
-                            <p>Nuevo King</p>
-                            {{-- Mensaje --}}
-                        @elseif ($message->type == 'userBoughtContent')
-                            <p>Comprador {{ $message->details['content']['id'] ?? '' }} -
-                                {{ $message->details['content']['type'] ?? '' }} - Cost:
-                                {{ $message->details['content']['cost'] ?? '' }}</p>
-                        @elseif($message->type == 'knightAdded')
-                            <p>King updated: {{ $message->details['knight']['username'] ?? '' }} - Level:
-                                {{ $message->details['knight']['userRanking']['level'] ?? '' }}</p>
-                        @elseif($message->type == 'userSubscribedOnFanClub')
-                            <p>Fan Club: {{ $message->details['fanClubSubscription']['user']['username'] ?? '' }} - Cost:
-                                {{ $message->details['fanClubSubscription']['tokens'] ?? '' }}</p>
-                        @else
-                            <p>{{ $message->details['body'] ?? dd($message->toArray()) }}</p>
-                        @endif
-                    @endif
+        @if ($messages->isEmpty())
+            <li class="timeline-item p-0">
+                <div class="d-flex justify-content-center">
+                    <p>No hay mensajes</p>
                 </div>
             </li>
-        @endforeach
+        @else
+            @foreach ($messages as $message)
+                <li class="{{ $message->isNew ? 'newMessage' : '' }}" wire:key="{{ $message->id }}">
+                    @if (($message->userData['isModel'] ?? false) && $message->owner_id === ($message->userData['id'] ?? null))
+                        @if (isset($message->details['lovenseDetails']))
+                            <div class="timeline-dots border-danger"></div>
+                        @elseif(isset($message->details['goal']))
+                            <div class="timeline-dots border-info"></div>
+                        @else
+                            <div class="timeline-dots border-success"></div>
+                        @endif
+                    @else
+                        <div class="timeline-dots"></div>
+                    @endif
+                    <div class="d-flex align-items-center justify-content-between">
+                        @if (isset($message->details['isAnonymous']) && $message->details['isAnonymous'])
+                            <h6 class="mb-1">Anonymous</h6>
+                        @else
+                            <h6 class="mb-1">{{ $message->userData['username'] ?? 'Anonymous' }}</h6>
+                        @endif
+                        <a href="{{ route('metadata', ['model' => 'chat', 'id' => base64_encode(json_encode($message->toArray()))]) }}"
+                            target="_blank">
+                            <small>{{ $message->elapsedTime }}</small>
+                        </a>
+                    </div>
+                    <div class="d-inline-block w-100">
+                        {{-- Mensaje owner --}}
+                        @if ($message->userData['isModel'] ?? false)
+                            {{-- Peticion Owner --}}
+                            @if (isset($message->details['goal']))
+                                <p>Objetivo: {{ $message->details['body'] ?? '' }} - {{ $message->details['goal'] }}</p>
+                                {{-- Respuesta owner --}}
+                            @elseif (isset($message->details['body']))
+                                <p>{{ $message->details['body'] }}</p>
+                                {{-- API --}}
+                            @elseif(isset($message->details['lovenseDetails']))
+                                @php
+                                    $love = $message->details['lovenseDetails']['detail'];
+                                @endphp
+                                @if (isset($love['power']))
+                                    <p>{{ $love['power'] }}({{ $love['time'] }} sec.) por <bold>{{ $love['name'] }}
+                                        </bold>
+                                        - propina {{ $love['amount'] }}</p>
+                                @elseif(isset($love['specialActualValue']) && $love['specialActualValue'] == 'giveControl')
+                                    <p>Control por {{ $love['name'] }}</p>
+                                @else
+                                    <p>Propina {{ $love['amount'] ?? dd($love) }} ({{ $love['time'] }} sec.)
+                                        {{ $love['name'] }}</p>
+                                @endif
+                                {{-- Propina --}}
+                            @else
+                                <p>Propina {{ $message->details['amount'] ?? '' }}</p>
+                            @endif
+                            {{-- Mensaje Regular --}}
+                        @else
+                            {{-- Propina --}}
+                            @if (isset($message->details['body']) && $message->details['body'] == '' && isset($message->details['amount']))
+                                <p>Propina {{ $message->details['amount'] }}</p>
+                                {{-- Nuevo King --}}
+                            @elseif($message->type == 'newKing')
+                                <p>Nuevo King</p>
+                                {{-- Mensaje --}}
+                            @elseif ($message->type == 'userBoughtContent')
+                                <p>Comprador {{ $message->details['content']['id'] ?? '' }} -
+                                    {{ $message->details['content']['type'] ?? '' }} - Cost:
+                                    {{ $message->details['content']['cost'] ?? '' }}</p>
+                            @elseif($message->type == 'knightAdded')
+                                <p>King updated: {{ $message->details['knight']['username'] ?? '' }} - Level:
+                                    {{ $message->details['knight']['userRanking']['level'] ?? '' }}</p>
+                            @elseif($message->type == 'userSubscribedOnFanClub')
+                                <p>Fan Club: {{ $message->details['fanClubSubscription']['user']['username'] ?? '' }} -
+                                    Cost:
+                                    {{ $message->details['fanClubSubscription']['tokens'] ?? '' }}</p>
+                            @else
+                                <p>{{ $message->details['body'] ?? dd($message->toArray()) }}</p>
+                            @endif
+                        @endif
+                    </div>
+                </li>
+            @endforeach
+        @endif
     </ul>
 </div>
